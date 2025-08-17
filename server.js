e70f8bf8c632d9066e9faea80e980ca32aa7e310
ash-veil <thyra.veil@gmail.com>
2025-08-17 10:22:36 +0100

    fix esModule warning

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ import 'dotenv/config'
 
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());/* 
-app.use(express.static(path.join(__dirname, 'public'))); */
+app.use(cookieParser());
 
 app.get('/', (req, res, next) => {
   res.send("Welcome to the server!");
